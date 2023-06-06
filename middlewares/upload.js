8e0049dd3bb2312d0da77e2ba896f3a1f9a4717a
Vadym <aspirantmpm@gmail.com>
2023-06-06 17:09:53 +0300

    filter

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,28 +1,35 @@
-const multer = require("multer");
-const path = require("path");
+const multer = require('multer');
+const path = require('path');
 
-const destination = path.resolve("temp");
+const { HttpError } = require('../helpers');
+
+const destination = path.resolve('temp');
 
 const storage = multer.diskStorage({
-    destination,
-    filename: (req, file, cb) => {
-        const uniquePrefix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        const newName = `${uniquePrefix}_${file.originalname}`;
-        cb(null, newName);
-    }
+  destination,
+  filename: (req, file, cb) => {
+    const uniquePrefix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    const newName = `${uniquePrefix}_${file.originalname}`;
+    cb(null, newName);
+  },
 });
 
 const limits = {
-    fileSize: 1024 * 1024
+  fileSize: 1024 * 1024,
 };
 
 const fileFilter = (req, file, cb) => {
-    console.log.apply(file);
-    cb(null, true);
+  const { mimetype } = file;
+  if (mimetype !== 'image/jpeg' || mimetype !== 'image/png') {
+    cb(HttpError(400, 'File can have only .jpg or .png extension'));
+  }
+  cb(null, true);
 };
 
 const upload = multer({
-    storage, limits, fileFilter,
+  storage,
+  limits,
+  fileFilter,
 });
 
 module.exports = upload;
